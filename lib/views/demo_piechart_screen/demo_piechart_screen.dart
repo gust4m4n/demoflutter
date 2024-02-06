48eba5298b15ccdf0b4c5864a840a383d68e06fa
Gustaman <>
2024-02-06 11:57:53 +0700

    Pie chat fixes

--- lib/views/demo_piechart_screen/demo_piechart_screen.dart
+++ lib/views/demo_piechart_screen/demo_piechart_screen.dart
@@ -6,29 +6,33 @@ class DemoPieChartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<DemoPieChartController>(
-      init: DemoPieChartController(),
-      builder: (controller) => ScreenX(
-          lightStatusBar: true,
-          headerView: NavigationBarX(
-            title: 'PieChart',
-            leftBtn: ImageX(
-              faIcon: FontAwesomeIcons.arrowLeft,
-              width: 20.0,
-              height: 20.0,
-              color: ColorX.white,
-              fit: BoxFit.contain,
-            ),
-            leftAction: () {
-              controller.btnBackClicked();
-            },
-          ),
-          bottomPadding: false,
-          bodyView: Container(
-            padding: const EdgeInsets.all(16.0),
-            child: PieChart(
-              PieChartData(sections: controller.pieChartData),
-            ),
-          )),
-    );
+        init: DemoPieChartController(),
+        builder: (controller) => ScreenX(
+              lightStatusBar: true,
+              headerView: NavigationBarX(
+                title: 'PieChart',
+                leftBtn: ImageX(
+                  faIcon: FontAwesomeIcons.arrowLeft,
+                  width: 20.0,
+                  height: 20.0,
+                  color: ColorX.white,
+                  fit: BoxFit.contain,
+                ),
+                leftAction: () {
+                  controller.btnBackClicked();
+                },
+              ),
+              bottomPadding: false,
+              bodyView: ContainerX(
+                  padding: const EdgeInsets.all(16.0),
+                  child: AspectRatio(
+                    aspectRatio: 1.0,
+                    child: PieChart(
+                      PieChartData(
+                        sections: controller.pieChartData,
+                      ),
+                    ),
+                  )),
+            ));
   }
 }

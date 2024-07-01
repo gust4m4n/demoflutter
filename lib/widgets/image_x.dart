36c03a885e650686483014ab88bcb7c33bc1d7f4
Gustaman <>
2024-07-01 10:48:57 +0700

    ImageX updates

--- lib/widgets/image_x.dart
+++ lib/widgets/image_x.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 import 'all_widgets.dart';
 
 class ImageX extends StatelessWidget {
@@ -15,6 +16,8 @@ class ImageX extends StatelessWidget {
   final String placeholder;
   final Color backgroundColor;
   final EdgeInsets padding;
+  final double borderWidth;
+  final Color borderColor;
 
   const ImageX({
     Key? key,
@@ -31,56 +34,51 @@ class ImageX extends StatelessWidget {
     this.placeholder = '',
     this.backgroundColor = ColorX.transparent,
     this.padding = const EdgeInsets.all(0.0),
+    this.borderWidth = 0.0,
+    this.borderColor = ColorX.transparent,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (url.trim().toLowerCase().startsWith('http://') ||
         url.trim().toLowerCase().startsWith('https://')) {
-      return ClipRRect(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(
-                  topLeftRadius > 0.0 ? topLeftRadius : cornerRadius),
-              topRight: Radius.circular(
-                  topRightRadius > 0.0 ? topLeftRadius : cornerRadius),
-              bottomLeft: Radius.circular(cornerRadius),
-              bottomRight: Radius.circular(cornerRadius)),
-          child: Container(
-            padding: padding,
-            color: backgroundColor,
+      return ContainerX(
+          padding: padding,
+          backgroundColor: backgroundColor,
+          borderWidth: borderWidth,
+          borderColor: borderColor,
+          cornerRadius: cornerRadius + borderWidth,
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(cornerRadius)),
             child: url.isNotEmpty
                 ? imageNetwork(context)
                 : imagePlaceholder(context),
           ));
     } else if (url.trim().toLowerCase().startsWith('file:')) {
-      return ClipRRect(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(
-                  topLeftRadius > 0.0 ? topLeftRadius : cornerRadius),
-              topRight: Radius.circular(
-                  topRightRadius > 0.0 ? topLeftRadius : cornerRadius),
-              bottomLeft: Radius.circular(cornerRadius),
-              bottomRight: Radius.circular(cornerRadius)),
-          child: Container(
-            padding: padding,
-            color: backgroundColor,
+      return ContainerX(
+          padding: padding,
+          backgroundColor: backgroundColor,
+          borderWidth: borderWidth,
+          borderColor: borderColor,
+          cornerRadius: cornerRadius + borderWidth,
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(cornerRadius)),
             child:
                 url.isNotEmpty ? imageFile(context) : imagePlaceholder(context),
           ));
     } else {
-      return ClipRRect(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(
-                  topLeftRadius > 0.0 ? topLeftRadius : cornerRadius),
-              topRight: Radius.circular(
-                  topRightRadius > 0.0 ? topLeftRadius : cornerRadius),
-              bottomLeft: Radius.circular(cornerRadius),
-              bottomRight: Radius.circular(cornerRadius)),
-          child: Container(
-            padding: padding,
-            color: backgroundColor,
+      return ContainerX(
+          padding: padding,
+          backgroundColor: backgroundColor,
+          borderWidth: borderWidth,
+          borderColor: borderColor,
+          cornerRadius: cornerRadius + borderWidth,
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(cornerRadius)),
             child: url.isNotEmpty
-                ? imageAsset(context, url)
+                ? url.startsWith('/')
+                    ? imageFile(context)
+                    : imageAsset(context, url)
                 : faIcon != null
                     ? imageAwesome(context, faIcon!)
                     : imagePlaceholder(context),
@@ -116,19 +114,17 @@ class ImageX extends StatelessWidget {
   Widget imageFile(BuildContext context) {
     var localPath = url.trim();
     const prefixLen = 'file:'.length;
-    if (localPath.length > prefixLen) {
+    if (localPath.startsWith('file:')) {
       localPath = localPath.substring(prefixLen, localPath.length);
-      return Image.file(File(localPath),
-          color: color,
-          width: width - (padding.left + padding.right),
-          height: height - (padding.top + padding.bottom),
-          fit: fit, errorBuilder:
-              (BuildContext context, Object exception, StackTrace? stackTrace) {
-        return imagePlaceholder(context);
-      });
-    } else {
-      return imagePlaceholder(context);
     }
+    return Image.file(File(localPath),
+        color: color,
+        width: width - (padding.left + padding.right),
+        height: height - (padding.top + padding.bottom),
+        fit: fit, errorBuilder:
+            (BuildContext context, Object exception, StackTrace? stackTrace) {
+      return imagePlaceholder(context);
+    });
   }
 
   Widget imageAsset(BuildContext context, String name) {
@@ -155,8 +151,8 @@ class ImageX extends StatelessWidget {
 
   Widget imageAwesome(BuildContext context, IconData icon) {
     return ContainerX(
-        width: width,
-        height: height,
+        width: width - (padding.left + padding.right),
+        height: height - (padding.top + padding.bottom),
         child: Center(
             child: FaIcon(
           icon,
